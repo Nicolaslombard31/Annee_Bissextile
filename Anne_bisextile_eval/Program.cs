fa3be491ee18d10380f414c34c595962b270a22c
Nicolaslombard31 <153175148+Nicolaslombard31@users.noreply.github.com>
2025-06-06 16:08:52 +0200

    consigne2

--- Anne_bisextile_eval/Program.cs
+++ Anne_bisextile_eval/Program.cs
@@ -7,6 +7,10 @@
             if (annee % 400 == 0){
                 return true;
             }
+            else if (annee % 100 == 0 && annee % 400 != 0)
+            {
+                return false;
+            }
             else
             {
                 return false;
