cfe187296222bbcd665fbb480a27c4b0b88bc5d7
Nicolaslombard31 <153175148+Nicolaslombard31@users.noreply.github.com>
2025-06-06 16:07:27 +0200

    consigne1

--- Anne_bisextile_eval/Program.cs
+++ Anne_bisextile_eval/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Annee_Bisextile
+{
+    public static class AnneeUtils
+    {
+        public static bool EstBissextile(int annee)
+        {
+            if (annee % 400 == 0){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
--- Annee_Bisextile/UnitTest1.cs
+++ Annee_Bisextile/UnitTest1.cs
@@ -1,11 +1,14 @@
+using Xunit;
+
 namespace Annee_Bisextile
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(400)]
+        public void AnneeDivissiblePar400EstBisextile(short a)
         {
-
+            var result = AnneeUtils.EstBissextile(a);
         }
     }
 }
